713fb2831643e4727abd48de2ede0449f8974335
cricket852 <97961216+cricket852@users.noreply.github.com>
2026-04-18 18:46:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,41 @@
-const CACHE_NAME = 'pokemon-dex-v2';
-const STATIC_ASSETS = ['/pokemon-dex/', '/pokemon-dex/index.html', '/pokemon-dex/manifest.json'];
+var CACHE_NAME = 'pokemon-dex-v2';
+var STATIC_ASSETS = ['/pokemon-dex/', '/pokemon-dex/index.html', '/pokemon-dex/manifest.json'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+    caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(STATIC_ASSETS); })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
+self.addEventListener('fetch', function(event) {
+  var url = new URL(event.request.url);
   if (url.hostname === 'pokeapi.co' || url.hostname === 'raw.githubusercontent.com') {
     event.respondWith(
-      caches.open(CACHE_NAME).then(async cache => {
-        const cached = await cache.match(event.request);
-        if (cached) return cached;
-        try {
-          const response = await fetch(event.request);
-          if (response.ok) cache.put(event.request, response.clone());
-          return response;
-        } catch {
-          return new Response('{}', { headers: { 'Content-Type': 'application/json' } });
-        }
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(cached) {
+          if (cached) return cached;
+          return fetch(event.request).then(function(response) {
+            if (response.ok) cache.put(event.request, response.clone());
+            return response;
+          }).catch(function() {
+            return new Response('{}', { headers: { 'Content-Type': 'application/json' } });
+          });
+        });
       })
     );
     return;
   }
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(function() { return caches.match(event.request); })
   );
 });
